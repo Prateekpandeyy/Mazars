cf2f2b90b975e081fa1ce485dc1c165e771443e8
Prateek <Pandeyprateek528@gmail.com>
2021-09-27 12:58:57 +0530

    done

--- src/pages/Admin/Report/Report.js
+++ src/pages/Admin/Report/Report.js
@@ -100,7 +100,6 @@ const Report = () => {
             placeholder="Enter type"
             ref={register({ required: true })}
           />
-          
         </div>
       </div>
     
@@ -140,7 +139,6 @@ const Report = () => {
                <label htmlFor="sno">S.No</label>
                </span>
                <span>
-
                <input type="checkbox" name="qno" ref={register} id="qno" checked disabled></input>
                <label htmlFor="qno">Query No</label>
                </span>
@@ -186,18 +184,16 @@ const Report = () => {
     <input type="checkbox" ref={register} name="t_requested" id="t_requested"></input>
 <label htmlFor="t_requested">Timeline Requested</label>
 </span>
-
 <span>  <input type="checkbox" ref={register} name="spc_que" id="spc_que"></input>
 <label htmlFor="spc_que">Specific questions</label>
 </span>
 <span>  <input type="checkbox" ref={register} name="doa" id="doa"></input>
 <label htmlFor="doa">Date of Allocation of Query</label>
 </span>
-            
-            </div>
-           </fieldset>
-           </div>
-   </div>
+</div>
+</fieldset>
+</div>
+</div>
 
 
    {/* Proposal */}
@@ -215,14 +211,13 @@ const Report = () => {
 <label htmlFor="proposedAmount">Proposed Amount</label>
 </span>
 <span>
-    <input type="checkbox"  ref={register} name="paymentTerms" id="paymentTerms"></input>
+<input type="checkbox"  ref={register} name="paymentTerms" id="paymentTerms"></input>
 <label htmlFor="paymentTerms">Payment Terms</label>
 </span>
 <span>
-    <input type="checkbox" ref={register} name="proposal_status" id="proposal_status"></input>
+<input type="checkbox" ref={register} name="proposal_status" id="proposal_status"></input>
 <label htmlFor="proposal_status">Proposal Status</label>
 </span>
-
 <span>  <input type="checkbox" ref={register} name="acceptedAmount" id="acceptedAmount"></input>
 <label htmlFor="acceptedAmount">Accepted Amount </label>
 </span>
@@ -232,9 +227,6 @@ const Report = () => {
 <span>  <input type="checkbox" ref={register} name="date_acceptance" id="date_acceptance"></input>
 <label htmlFor="date_acceptance">Date of Acceptance / Decline</label>
 </span>
-            
-            
-
 <span>
 <input type="checkbox" ref={register} name="amountReceived" id="amountReceived"></input>
 <label htmlFor="amountReceived">Total Amount Received</label>
@@ -247,27 +239,20 @@ const Report = () => {
     <input type="checkbox" ref={register} name="amount_overdue" id="amount_overdue"></input>
 <label htmlFor="amount_overdue">Total Amount Overdue</label>
 </span>
-
-
 <span>  <input type="checkbox" ref={register} name="declinedDate" id="declinedDate"></input>
 <label htmlFor="declinedDate">Payment decline date</label>
 </span>             
-            </div>
-      
+            </div>      
            </fieldset>
            </div>
    </div>
-
 
    {/* Assignment */}
    <div className="row">
        <div className="col-md-12">
        <fieldset className="my-fieldset">
            <legend className="login-legend">Assignment</legend>
-         
-
             <div className="basicFeild">
-            
             <span>
 <input type="checkbox" ref={register} name="assignNumber" id="assignNumber"></input>
 <label htmlFor="assignNumber">Assignment Number</label>
@@ -291,10 +276,7 @@ const Report = () => {
 <span>  <input type="checkbox" ref={register} name="assignTime" id="assignTime"></input>
 <label htmlFor="assignTime">Time taken to complete the assignment</label>
 </span>
-           
-            </div>
-
-           
+            </div>    
            </fieldset>
            </div>
    </div>
@@ -305,22 +287,17 @@ const Report = () => {
    <div className="row">
        <div className="col-md-12">
        <fieldset className="my-fieldset">
-           <legend className="login-legend">Payment Receipt</legend>
-            
+           <legend className="login-legend">Payment Receipt</legend>  
             <div className="basicFeild">
             <span>
 <input type="checkbox" ref={register} name="receiptDate" id="receiptDate"></input>
 <label htmlFor="receiptDate">Date of Receipt</label>
 </span>   
-
 <span>
 <input type="checkbox" ref={register} name="amountReceived" id="amountReceived"></input>
 <label htmlFor="amountReceived">Amount Received</label>
 </span>
-       
             </div>
-
-           
            </fieldset>
            </div>
    </div>
@@ -330,7 +307,6 @@ const Report = () => {
   </div>
             </Layout>
               </>
-   
        );
    }
 export default Report;
