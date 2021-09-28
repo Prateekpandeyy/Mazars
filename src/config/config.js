dfac0fb63dfbd1b2aece833b75a42654a4fafaab
Prateek <Pandeyprateek528@gmail.com>
2021-09-28 17:01:23 +0530

    done

--- src/config/config.js
+++ src/config/config.js
@@ -1,51 +1,23 @@
-
 // http://13.232.121.233/mazarsapi/v1/
 
 
 
 
 //for local
-// export const baseUrl = "http://13.232.121.233/mazarsapi/v1";
-// export const ImageUrl = "http://13.232.121.233/mazarsapi/assets/image";
-// export const ReportUrl = "http://13.232.121.233/mazarsapi/assets/upload/report";
+export const baseUrl = "http://13.232.121.233/mazarsapi/v1";
+export const ImageUrl = "http://13.232.121.233/mazarsapi/assets/image";
+export const ReportUrl = "http://13.232.121.233/mazarsapi/assets/upload/report";
 
 
 
 
 // //for server
-export const baseUrl = "https://mazarsapi.multitvsolution.com/mazarapi/v1";
-export const ImageUrl = "https://mazarsapi.multitvsolution.com/mazarapi/assets/image";
-export const ReportUrl = "https://mazarsapi.multitvsolution.com/mazarapi/assets/upload/report";
+// export const baseUrl = "https://mazarsapi.multitvsolution.com/mazarapi/v1";
+// export const ImageUrl = "https://mazarsapi.multitvsolution.com/mazarapi/assets/image";
+// export const ReportUrl = "https://mazarsapi.multitvsolution.com/mazarapi/assets/upload/report";
 
 // for local
- // export const baseUrl2 = "http://13.232.121.233";
+   export const baseUrl2 = "http://13.232.121.233";
 
 // for server 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-  export const baseUrl2 = "https://mazarsapi.multitvsolution.com"
-=======
-<<<<<<< HEAD
- // export const baseUrl2 = "https://mazarsapi.multitvsolution.com"
->>>>>>> d84b9c3289cc7a587ee8dde0acfec1a013dd1747
-=======
-  export const baseUrl2 = "https://mazarsapi.multitvsolution.com"
->>>>>>> parent of 9c699461e... done
-=======
-  export const baseUrl2 = "https://mazarsapi.multitvsolution.com"
->>>>>>> parent of 9c699461e... done
-
-
-
-
-
-
-
-
-<<<<<<< HEAD
-=======
-=======
  //  export const baseUrl2 = "https://mazarsapi.multitvsolution.com"
->>>>>>> dev_jyoti
->>>>>>> d84b9c3289cc7a587ee8dde0acfec1a013dd1747
