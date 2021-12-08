59db1c3978b095cbd3d2743003c9f0794d2d8863
Badboyyyy <pandeyprateek528@gmail.com>
2021-12-08 11:52:53 +0530

    done

--- src/config/http/http-common.js
+++ src/config/http/http-common.js
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
-import axios from "axios";
-=======
+
  import axios from "axios";
->>>>>>> 9bb0a9ca5bffb1c6c093c95cb7c0f2e1e21214e9
+
 export default axios.create({
   baseURL: "http://13.232.121.233/mazarsapi/v1",
   headers: {
