72e2e52d47decef337937faf2c4865e5fcee3846
Badboyyyy <pandeyprateek528@gmail.com>
2022-01-25 08:48:22 +0530

    done

--- src/views/PaymentStatus/Unpaid.js
+++ src/views/PaymentStatus/Unpaid.js
@@ -363,19 +363,15 @@ function Paid() {
                   style={{ cursor: "pointer", margin: "0 3px" }}
                   title="Pay Amount"
                  >
-                    <Link
-              to={{
-                pathname: `/customer/paydetails/${row.assign_id}`,
-                obj: {
-                  message_type: "5",
-                  query_No: row.assign_no,
-                  query_id: row.q_id,
-                  routes: `/customer/payment`
-                }
-              }}
-            >
-                                      <PaymentIcon color="primary" />
-                  </Link>
+                     <Link
+                            to={{
+                                pathname: `/customer/paydetails/${row.assign_id}`,
+                                index : 2,
+                                routes: "paymentstatus",
+                            }}
+                        >
+<PaymentIcon color="primary" />
+                        </Link>
                 </div>
                 :
                 null
@@ -387,18 +383,14 @@ function Paid() {
               row.paid_amount > 0 && row.paid_status > 0 ?
                 <div style={{ cursor: "pointer", margin: "0 5px" }} title="Payment History">
                <Link
-              to={{
-                pathname: `/customer/paydetails/${row.assign_id}`,
-                obj: {
-                  message_type: "5",
-                  query_No: row.assign_no,
-                  query_id: row.q_id,
-                  routes: `/customer/payment`
-                }
-              }}
-            >
-                               <PaymentIcon color="primary" />
-                  </Link>    
+                            to={{
+                                pathname: `/customer/paydetails/${row.assign_id}`,
+                                index : 2,
+                                routes: "paymentstatus",
+                            }}
+                        >
+<PaymentIcon color="primary" />
+                        </Link>    
                 </div>
                 :
                 null
