440d6dee7fce2b043308ee37dc119d368475294e
Prateek <Pandeyprateek528@gmail.com>
2022-10-03 15:33:16 +0530

    done

--- src/pages/TL/EditProposal/EditProposal.js
+++ src/pages/TL/EditProposal/EditProposal.js
@@ -96,6 +96,7 @@ const [scopeError, setScopeError] = useState(false)
   })
   const [invoiceAmount, setinvoiceAmount] = useState(0);
   const [installmentAmount, sentInstallmentAmount] = useState(0);
+  const [no_installmentRange, setno_installment] = useState([])
 const token = window.localStorage.getItem("tlToken")
   const myConfig = {
       headers : {
@@ -149,6 +150,7 @@ let adjustAmount = 0;
     axios.get(`${baseUrl}/tl/getProposalDetail?id=${id}`, myConfig).then((res) => {
 
       if (res.data.code === 1) {
+       
         mainAmount = res.data.result.installment_amount.split(",")
         mainDueDate = res.data.result.due_date.split(",");
         console.log("mainDueDate", mainDueDate)
@@ -181,7 +183,7 @@ let adjustAmount = 0;
             invoiceAmount : invoiceAmount,
             // remainAmount : Number(res.data.result.amount) - Number(invoiceAmount)
            })
-        
+       
         due_date.map((i, e) =>{
        
           mainDueDate[e] = i;
@@ -229,7 +231,16 @@ let adjustAmount = 0;
           
       
         }
+        for(let i = installment_number.length; i < 37; i++){
          
+          let install = {
+            value : String(i),
+            label : String(i)
+          }
+          setno_installment((oldData) => {
+          return  [...oldData, install]
+          })
+        }
       
         if(res.data.result.email.length > 0){
        
@@ -655,7 +666,7 @@ const getSubPlan  = (e) => {
   setInstallment([])
   setSubplan(e.target.value)
 }
-
+console.log("no_installmentRange", no_installmentRange)
   return (
     <Layout TLDashboard="TLDashboard" TLuserId={userid}>
       <Card>
@@ -1338,6 +1349,7 @@ setInstallment({
                  <div class="form-group">
                       <label>No of installments</label>
                       <Select
+                      isSearchable={false}
                         onChange={(e => installmentHandler(e))}
                         value = {installment}
                         options={no_installmentRange}
@@ -1545,146 +1557,146 @@ const noInstallments = [
     label: "4",
   },
 ];
-const no_installmentRange = [
-  {
-    value: "2",
-    label: "2",
-  },
-  {
-    value: "3",
-    label: "3",
-  },
-  {
-    value: "4",
-    label: "4",
-  },
-  {
-    value: "5",
-    label: "5",
-  },
-  {
-    value: "6",
-    label: "6",
-  },
-  {
-    value: "7",
-    label: "7",
-  },
-  {
-    value: "8",
-    label: "8",
-  },
-  {
-    value: "9",
-    label: "9",
-  },
-  {
-    value: "10",
-    label: "10",
-  },
-  {
-    value: "11",
-    label: "11",
-  },
-  {
-    value: "12",
-    label: "12",
-  },
-  {
-    value: "13",
-    label: "13",
-  },
-  {
-    value: "14",
-    label: "14",
-  },
-  {
-    value: "15",
-    label: "15",
-  },
-  {
-    value: "16",
-    label: "16",
-  },
-  {
-    value: "17",
-    label: "17",
-  },
-  {
-    value: "18",
-    label: "18",
-  },
-  {
-    value: "19",
-    label: "19",
-  },
-  {
-    value: "20",
-    label: "20",
-  },
-  {
-    value: "21",
-    label: "21",
-  },
-  {
-    value: "22",
-    label: "22",
-  },
-  {
-    value: "23",
-    label: "23",
-  },
-  {
-    value: "24",
-    label: "24",
-  },
-  {
-    value: "25",
-    label: "25",
-  },
-  {
-    value: "26",
-    label: "26",
-  },
-  {
-    value: "27",
-    label: "27",
-  },
-  {
-    value: "28",
-    label: "28",
-  },
-  {
-    value: "29",
-    label: "29",
-  },
-  {
-    value: "30",
-    label: "30",
-  },
-  {
-    value: "31",
-    label: "31",
-  },
-  {
-    value: "32",
-    label: "32",
-  },
-  {
-    value: "33",
-    label: "33",
-  },
-  {
-    value: "34",
-    label: "34",
-  },
-  {
-    value: "35",
-    label: "35",
-  },
-  {
-    value: "36",
-    label: "36",
-  },
+// const no_installmentRange = [
+//   {
+//     value: "2",
+//     label: "2",
+//   },
+//   {
+//     value: "3",
+//     label: "3",
+//   },
+//   {
+//     value: "4",
+//     label: "4",
+//   },
+//   {
+//     value: "5",
+//     label: "5",
+//   },
+//   {
+//     value: "6",
+//     label: "6",
+//   },
+//   {
+//     value: "7",
+//     label: "7",
+//   },
+//   {
+//     value: "8",
+//     label: "8",
+//   },
+//   {
+//     value: "9",
+//     label: "9",
+//   },
+//   {
+//     value: "10",
+//     label: "10",
+//   },
+//   {
+//     value: "11",
+//     label: "11",
+//   },
+//   {
+//     value: "12",
+//     label: "12",
+//   },
+//   {
+//     value: "13",
+//     label: "13",
+//   },
+//   {
+//     value: "14",
+//     label: "14",
+//   },
+//   {
+//     value: "15",
+//     label: "15",
+//   },
+//   {
+//     value: "16",
+//     label: "16",
+//   },
+//   {
+//     value: "17",
+//     label: "17",
+//   },
+//   {
+//     value: "18",
+//     label: "18",
+//   },
+//   {
+//     value: "19",
+//     label: "19",
+//   },
+//   {
+//     value: "20",
+//     label: "20",
+//   },
+//   {
+//     value: "21",
+//     label: "21",
+//   },
+//   {
+//     value: "22",
+//     label: "22",
+//   },
+//   {
+//     value: "23",
+//     label: "23",
+//   },
+//   {
+//     value: "24",
+//     label: "24",
+//   },
+//   {
+//     value: "25",
+//     label: "25",
+//   },
+//   {
+//     value: "26",
+//     label: "26",
+//   },
+//   {
+//     value: "27",
+//     label: "27",
+//   },
+//   {
+//     value: "28",
+//     label: "28",
+//   },
+//   {
+//     value: "29",
+//     label: "29",
+//   },
+//   {
+//     value: "30",
+//     label: "30",
+//   },
+//   {
+//     value: "31",
+//     label: "31",
+//   },
+//   {
+//     value: "32",
+//     label: "32",
+//   },
+//   {
+//     value: "33",
+//     label: "33",
+//   },
+//   {
+//     value: "34",
+//     label: "34",
+//   },
+//   {
+//     value: "35",
+//     label: "35",
+//   },
+//   {
+//     value: "36",
+//     label: "36",
+//   },
  
-];
+// ];
