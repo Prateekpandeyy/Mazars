461786a9adc86b2de9f390ed810347d69aef1ebe
Prateek <Pandeyprateek528@gmail.com>
2022-11-11 10:41:45 +0530

    done

--- src/components/QueryDetails/Folder/ShowFolder.js
+++ src/components/QueryDetails/Folder/ShowFolder.js
@@ -13,7 +13,7 @@ import axios from "axios";
 import { baseUrl } from "../../../config/config";
 import Select from "react-select";
 import "reactjs-popup/dist/index.css";
-
+import Swal from "sweetalert2";
 const FolderWrapper = styled(Box)({
   display: "flex",
   width: "50%",
@@ -39,6 +39,8 @@ function ShowFolder({ addPaymentModal, rejectHandler, id }) {
   const [movedFolder, setMovedFolder] = useState([]);
   const [files, setFiles] = useState([]);
   const [folderId, setFolderId] = useState([]);
+  const [fileId, setFileId] = useState("");
+  const [innerFiles, setInnerFiles] = useState([]);
   const uid = localStorage.getItem("tlkey");
   const token = window.localStorage.getItem("tlToken");
 
@@ -47,7 +49,19 @@ function ShowFolder({ addPaymentModal, rejectHandler, id }) {
       uit: token,
     },
   };
-  const getList = () => {
+  const getFile = () => {
+    axios
+      .get(
+        `${baseUrl}/tl/documentlistbyfolder?q_id=${id}&uid=${JSON.parse(uid)}`,
+        myConfig
+      )
+      .then((res) => {
+        if (res.data.code === 1) {
+          setFiles(res.data.result);
+        }
+      });
+  };
+  const showFolder = () => {
     let kk = [];
     let movedFold = {};
     axios
@@ -70,49 +84,81 @@ function ShowFolder({ addPaymentModal, rejectHandler, id }) {
       });
   };
   useEffect(() => {
-    getList();
+    showFolder();
+    getFile();
   }, [id]);
 
   const getFolder = (e) => {
     setCreateFolder(!createFoldernew);
   };
-  const onDrag = (e) => {};
-  // get file with in folder
-  const getFile = (e) => {
-    setColor(e.id);
-
-    axios
-      .get(
-        `${baseUrl}/tl/documentlistbyfolder?q_id=${id}&folder_id=${e.id}`,
-        myConfig
-      )
-      .then((res) => {
-        if (res.data.code === 1) {
-          setFiles(res.data.result);
-        }
-      });
-    setShowDetail(true);
-  };
   const handleFile = (e) => {
-    setMove(!move);
+    if (e) {
+      setFileId(e.id);
+      setMove(!move);
+    } else {
+      setMove(!move);
+    }
   };
   const mapIcon = (e) => {
     axios
       .get(
-        `${baseUrl}/tl/ArticleIcon?q_id=${id}&folder_id=${folderId.value}&file_id=1`,
+        `${baseUrl}/tl/folderfile?q_id=${id}&folder_id=${folderId.value}&file_id=${fileId}`,
         myConfig
       )
       .then((res) => {
-        console.log("response", res);
+        if (res.data.code === 1) {
+          handleFile();
+          showFolder();
+          getFile();
+          setInnerFiles([]);
+          setColor(0);
+          Swal.fire({
+            title: "success",
+            html: "File transfered successfully",
+            icons: "success",
+          });
+        } else if (res.data.code === 0) {
+          Swal.fire({
+            title: "error",
+            html: "Something went wrong, please try again",
+            icons: "error",
+          });
+        }
+      })
+      .catch((error) => {
+        Swal.fire({
+          title: "error",
+          html: error,
+          icons: "error",
+        });
       });
   };
-  console.log(folderId);
+  const getInnerFileFile = (e) => {
+    axios
+      .get(
+        `${baseUrl}/tl/documentlistbyfolder?q_id=${id}&folder_id=${
+          e.id
+        }&uid=${JSON.parse(uid)}`,
+        myConfig
+      )
+      .then((res) => {
+        if (res.data.code === 1) {
+          setColor(e.id);
+          setInnerFiles(res.data.result);
+        }
+      });
+  };
   return (
     <div>
-      <Modal isOpen={addPaymentModal} toggle={rejectHandler} size="xl">
+      <Modal
+        isOpen={addPaymentModal}
+        toggle={rejectHandler}
+        size="lg"
+        scrollable
+      >
         <ModalHeader toggle={rejectHandler}>
           <div className="d-flex w-100 justify-content-between">
-            <CustomHeading>Folder</CustomHeading>
+            <CustomHeading>Document</CustomHeading>
             <button className="autoWidthBtn" onClick={(e) => getFolder()}>
               {" "}
               <CreateNewFolderIcon style={{ margin: "0px 5px" }} /> Create
@@ -126,7 +172,7 @@ function ShowFolder({ addPaymentModal, rejectHandler, id }) {
               <div className="folderCreated">
                 {color === i.id ? (
                   <FolderIcon
-                    onClick={(e) => getFile(i)}
+                    onClick={(e) => getInnerFileFile(i)}
                     style={{
                       fontSize: "50px",
                       color: "#0000ff",
@@ -135,7 +181,7 @@ function ShowFolder({ addPaymentModal, rejectHandler, id }) {
                   />
                 ) : (
                   <FolderIcon
-                    onClick={(e) => getFile(i)}
+                    onClick={(e) => getInnerFileFile(i)}
                     style={{
                       fontSize: "50px",
                       color: "#fccc77",
@@ -150,45 +196,66 @@ function ShowFolder({ addPaymentModal, rejectHandler, id }) {
                 </span>
               </div>
             ))}
+            {files.map((i) => (
+              <>
+                {i.folder_id === "0" ? (
+                  <div className="folderCreated">
+                    <ArticleIcon
+                      onClick={(e) => handleFile(i)}
+                      style={{
+                        fontSize: "50px",
+                        color: "#0000ff",
+                        cursor: "pointer",
+                      }}
+                    />
+                    <span
+                      style={{
+                        textAlign: "center",
+                        whiteSpace: "break-spaces",
+                      }}
+                    >
+                      {i.name}
+                    </span>
+                  </div>
+                ) : (
+                  ""
+                )}
+              </>
+            ))}
           </FolderWrapper>
-          {showDetail === true ? (
-            <FolderDetails>
-              <div className="folderDetails">
-                <ArticleIcon
-                  onClick={(e) => handleFile(e)}
-                  style={{
-                    fontSize: "50px",
-                    color: "#fccc77",
-                    cursor: "pointer",
-                  }}
-                />
-                <span>Test</span>
-              </div>
 
-              <div className="folderDetails">
-                <ArticleIcon style={{ fontSize: "50px", cursor: "pointer" }} />{" "}
-                <span>RAtee</span>
-              </div>
-              <div className="folderDetails">
-                <ImageIcon style={{ fontSize: "50px", cursor: "pointer" }} />{" "}
-                <span>RAtee</span>
-              </div>
-              <div className="folderDetails">
-                <AudioFileIcon
-                  style={{ fontSize: "50px", cursor: "pointer" }}
-                />{" "}
-                <span>RAtee</span>
-              </div>
-            </FolderDetails>
-          ) : (
-            ""
-          )}
+          <FolderDetails>
+            <div className="folderDetails d-flex">
+              {innerFiles.map((i) => (
+                <>
+                  <div className="folderCreated">
+                    <ArticleIcon
+                      onClick={(e) => handleFile(i)}
+                      style={{
+                        fontSize: "50px",
+                        color: "#0000ff",
+                        cursor: "pointer",
+                      }}
+                    />
+                    <span
+                      style={{
+                        textAlign: "center",
+                        whiteSpace: "break-spaces",
+                      }}
+                    >
+                      {i.name}
+                    </span>
+                  </div>
+                </>
+              ))}
+            </div>
+          </FolderDetails>
         </ModalBody>
       </Modal>
       <CreateFolder
         addPaymentModal={createFoldernew}
         id={id}
-        getList={getList}
+        getList={showFolder}
         rejectHandler={getFolder}
       />
       {move === true ? (
