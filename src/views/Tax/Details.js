33b739c95e325f242f2c405d1d353a3c4a3c556a
Prateek <Pandeyprateek528@gmail.com>
2022-12-24 11:36:55 +0530

    article inner in layout

--- src/views/Tax/Details.js
+++ src/views/Tax/Details.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import Header from "../../components/Header/Header";
 import { styled } from "@material-ui/styles";
 import Footer from "../../components/Footer/Footer";
 import axios from "axios";
@@ -11,6 +10,7 @@ import classes from "./design.module.css";
 import { OuterloginContainer } from "../../components/Common/OuterloginContainer";
 import { Link, useHistory } from "react-router-dom";
 import { VscFilePdf } from "react-icons/vsc";
+import Layout from "../../components/Layout/Layout";
 import { Viewer } from "@react-pdf-viewer/core"; // install this library
 // Plugins
 // import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout'; // install this library
@@ -32,7 +32,7 @@ const ArticleWrapper = styled(Box)({
 const Details = () => {
   let history = useHistory();
   const [data, setData] = useState([]);
-
+  const userId = window.localStorage.getItem("userid");
   const getData = (e) => {
     $("#artContent a[href^='http://']").attr("target", "_blank");
     if (history.location.index !== undefined) {
@@ -47,90 +47,95 @@ const Details = () => {
     getData();
   }, []);
   return (
-    <>
-      <OuterloginContainer>
-        <Header noSign="noSign" />
-        <MyContainer>
-          <div className={classes.articleContent}>
-            {data.map((i) => (
-              <div className={classes.articlesDetails}>
-                <Breadcrumbs
-                  separator=">"
-                  maxItems={3}
-                  aria-label="breadcrumb"
-                  style={{ fontSize: "18px" }}
-                >
-                  <Link underline="hover" color="inherit" to="/customer/direct">
-                    Articles
-                  </Link>
-                </Breadcrumbs>
-                <ArticleWrapper>
-                  <MainText>
-                    {CommonServices.capitalizeFirstLetter(i.heading)}
-                  </MainText>
+    <Layout custDashboard="custDashboard" custUserId={userId}>
+      <>
+        <OuterloginContainer>
+          <MyContainer>
+            <div className={classes.articleContent}>
+              {data.map((i) => (
+                <div className={classes.articlesDetails}>
+                  <Breadcrumbs
+                    separator=">"
+                    maxItems={3}
+                    aria-label="breadcrumb"
+                    style={{ fontSize: "18px" }}
+                  >
+                    <Link
+                      underline="hover"
+                      color="inherit"
+                      to="/customer/direct"
+                    >
+                      Articles
+                    </Link>
+                  </Breadcrumbs>
+                  <ArticleWrapper>
+                    <MainText>
+                      {CommonServices.capitalizeFirstLetter(i.heading)}
+                    </MainText>
 
-                  <MainText style={{ margin: "8px 0px 12px 0px" }}>
-                    Date of publishing :{" "}
-                    {i.publish_date.split("-").reverse().join("-")}
-                  </MainText>
+                    <MainText style={{ margin: "8px 0px 12px 0px" }}>
+                      Date of publishing :{" "}
+                      {i.publish_date.split("-").reverse().join("-")}
+                    </MainText>
 
-                  {i.content_type === "2" ? (
-                    <div id="artContent" className="updatesContent">
-                      <Markup content={i.content} />
-                    </div>
-                  ) : (
-                    " "
-                  )}
-                  {i.content_type === "0" || i.content_type === "1" ? (
-                    <div id="artContent">
-                      <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js">
-                        <Viewer fileUrl={`${baseUrl3}/${i.file}`}></Viewer>
-                      </Worker>
-                    </div>
-                  ) : (
-                    ""
-                  )}
+                    {i.content_type === "2" ? (
+                      <div id="artContent" className="updatesContent">
+                        <Markup content={i.content} />
+                      </div>
+                    ) : (
+                      " "
+                    )}
+                    {i.content_type === "0" || i.content_type === "1" ? (
+                      <div id="artContent">
+                        <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js">
+                          <Viewer fileUrl={`${baseUrl3}/${i.file}`}></Viewer>
+                        </Worker>
+                      </div>
+                    ) : (
+                      ""
+                    )}
 
-                  {i.content_type === "3" ? (
-                    <div id="artContent">
-                      <iframe
-                        src={`https://view.officeapps.live.com/op/embed.aspx?src=${baseUrl3}/${i.file}`}
-                        width="100%"
-                        height="600px"
-                        frameBorder="0"
-                        title="slides"
-                      ></iframe>
-                    </div>
-                  ) : (
-                    ""
-                  )}
-                </ArticleWrapper>
-                <a
-                  href={`${baseUrl3}/${i.file}`}
-                  target="_blank"
-                  className={classes.myLink}
-                >
-                  <button className="downloadBtnPdf">
-                    {" "}
-                    Download{" "}
-                    <VscFilePdf
-                      style={{
-                        display: "flex",
-                        margin: "0 10px",
-                        color: "#e4f0fa",
-                        width: "20px",
-                        height: "20px",
-                      }}
-                    />
-                  </button>
-                </a>
-              </div>
-            ))}
-          </div>
-        </MyContainer>
-        <Footer />
-      </OuterloginContainer>
-    </>
+                    {i.content_type === "3" ? (
+                      <div id="artContent">
+                        <iframe
+                          src={`https://view.officeapps.live.com/op/embed.aspx?src=${baseUrl3}/${i.file}`}
+                          width="100%"
+                          height="600px"
+                          frameBorder="0"
+                          title="slides"
+                        ></iframe>
+                      </div>
+                    ) : (
+                      ""
+                    )}
+                  </ArticleWrapper>
+                  <a
+                    href={`${baseUrl3}/${i.file}`}
+                    target="_blank"
+                    className={classes.myLink}
+                  >
+                    <button className="downloadBtnPdf">
+                      {" "}
+                      Download{" "}
+                      <VscFilePdf
+                        style={{
+                          display: "flex",
+                          margin: "0 10px",
+                          color: "#e4f0fa",
+                          width: "20px",
+                          height: "20px",
+                        }}
+                      />
+                    </button>
+                  </a>
+                </div>
+              ))}
+            </div>
+          </MyContainer>
+          <Footer />
+        </OuterloginContainer>
+      </>
+    </Layout>
   );
 };
 export default Details;
--- src/views/Tax/UpdateDetails.js
+++ src/views/Tax/UpdateDetails.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import Header from "../../components/Header/Header";
 import { styled } from "@material-ui/styles";
 import Footer from "../../components/Footer/Footer";
 import axios from "axios";
@@ -16,6 +15,7 @@ import "@react-pdf-viewer/core/lib/styles/index.css";
 import { Worker } from "@react-pdf-viewer/core"; // install this library
 import MyContainer from "../../components/Common/MyContainer";
 import MainText from "../../components/Common/MainText";
+import Layout from "../../components/Layout/Layout";
 const ArticleWrapper = styled(Box)({
   display: "flex",
   flexDirection: "column",
@@ -26,6 +26,7 @@ const ArticleWrapper = styled(Box)({
 const UpdateDetails = () => {
   const [data, setData] = useState([]);
   let id = useParams();
+  const userId = window.localStorage.getItem("userid");
   let history = useHistory();
 
   const getData = (e) => {
@@ -37,117 +38,118 @@ const UpdateDetails = () => {
     getData();
   }, []);
   return (
-    <>
-      <OuterloginContainer>
-        <Header noSign="noSign" />
-        <MyContainer>
-          <div className={classes.articleContent}>
-            {data.map((i) => (
-              <div className={classes.articlesDetails}>
-                {history.location.index && (
-                  <Breadcrumbs
-                    separator=">"
-                    maxItems={3}
-                    aria-label="breadcrumb"
-                    style={{ fontSize: "18px" }}
-                  >
-                    <Link
-                      underline="hover"
-                      color="inherit"
-                      to="/customer/updatedirect"
+    <Layout custDashboard="custDashboard" custUserId={userId}>
+      <>
+        <OuterloginContainer>
+          <MyContainer>
+            <div className={classes.articleContent}>
+              {data.map((i) => (
+                <div className={classes.articlesDetails}>
+                  {history.location.index && (
+                    <Breadcrumbs
+                      separator=">"
+                      maxItems={3}
+                      aria-label="breadcrumb"
+                      style={{ fontSize: "18px" }}
                     >
-                      Update
-                    </Link>
-
-                    {history.location.index === "miscellaneous" ? (
                       <Link
                         underline="hover"
                         color="inherit"
-                        to={`/customer/${history.location.index}`}
+                        to="/customer/updatedirect"
                       >
-                        Miscellaneous
+                        Update
                       </Link>
+
+                      {history.location.index === "miscellaneous" ? (
+                        <Link
+                          underline="hover"
+                          color="inherit"
+                          to={`/customer/${history.location.index}`}
+                        >
+                          Miscellaneous
+                        </Link>
+                      ) : (
+                        <Link
+                          underline="hover"
+                          color="inherit"
+                          to={`/customer/update${history.location.index}`}
+                        >
+                          {CommonServices.capitalizeFirstLetter(
+                            history.location.index
+                          ) + " tax"}
+                        </Link>
+                      )}
+                    </Breadcrumbs>
+                  )}
+                  <ArticleWrapper>
+                    <MainText>
+                      {CommonServices.capitalizeFirstLetter(i.heading)}
+                    </MainText>
+
+                    <MainText style={{ margin: "8px 0px 12px 0px" }}>
+                      Date of publishing :{" "}
+                      {i.publish_date.split("-").reverse().join("-")}
+                    </MainText>
+
+                    {i.content_type === "2" ? (
+                      <div id="artContent" className="updatesContent">
+                        <Markup content={i.content} />
+                      </div>
                     ) : (
-                      <Link
-                        underline="hover"
-                        color="inherit"
-                        to={`/customer/update${history.location.index}`}
-                      >
-                        {CommonServices.capitalizeFirstLetter(
-                          history.location.index
-                        ) + " tax"}
-                      </Link>
+                      " "
                     )}
-                  </Breadcrumbs>
-                )}
-                <ArticleWrapper>
-                  <MainText>
-                    {CommonServices.capitalizeFirstLetter(i.heading)}
-                  </MainText>
+                    {i.content_type === "0" || i.content_type === "1" ? (
+                      <div id="artContent">
+                        <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js">
+                          <Viewer fileUrl={`${baseUrl3}/${i.file}`}></Viewer>
+                        </Worker>
+                      </div>
+                    ) : (
+                      ""
+                    )}
 
-                  <MainText style={{ margin: "8px 0px 12px 0px" }}>
-                    Date of publishing :{" "}
-                    {i.publish_date.split("-").reverse().join("-")}
-                  </MainText>
-
-                  {i.content_type === "2" ? (
-                    <div id="artContent" className="updatesContent">
-                      <Markup content={i.content} />
-                    </div>
-                  ) : (
-                    " "
-                  )}
-                  {i.content_type === "0" || i.content_type === "1" ? (
-                    <div id="artContent">
-                      <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js">
-                        <Viewer fileUrl={`${baseUrl3}/${i.file}`}></Viewer>
-                      </Worker>
-                    </div>
-                  ) : (
-                    ""
-                  )}
-
-                  {i.content_type === "3" ? (
-                    <div id="artContent">
-                      <iframe
-                        src={`https://view.officeapps.live.com/op/embed.aspx?src=${baseUrl3}/${i.file}`}
-                        width="100%"
-                        height="600px"
-                        frameBorder="0"
-                        title="slides"
-                      ></iframe>
-                    </div>
-                  ) : (
-                    ""
-                  )}
-                </ArticleWrapper>
-                <a
-                  href={`${baseUrl3}/${i.file}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className={classes.myLink}
-                >
-                  <button className="downloadBtnPdf">
-                    {" "}
-                    Download{" "}
-                    <VscFilePdf
-                      style={{
-                        display: "flex",
-                        margin: "0 10px",
-                        color: "#e4f0fa",
-                        width: "20px",
-                        height: "20px",
-                      }}
-                    />
-                  </button>
-                </a>
-              </div>
-            ))}
-          </div>
-        </MyContainer>
-        <Footer />
-      </OuterloginContainer>
-    </>
+                    {i.content_type === "3" ? (
+                      <div id="artContent">
+                        <iframe
+                          src={`https://view.officeapps.live.com/op/embed.aspx?src=${baseUrl3}/${i.file}`}
+                          width="100%"
+                          height="600px"
+                          frameBorder="0"
+                          title="slides"
+                        ></iframe>
+                      </div>
+                    ) : (
+                      ""
+                    )}
+                  </ArticleWrapper>
+                  <a
+                    href={`${baseUrl3}/${i.file}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className={classes.myLink}
+                  >
+                    <button className="downloadBtnPdf">
+                      {" "}
+                      Download{" "}
+                      <VscFilePdf
+                        style={{
+                          display: "flex",
+                          margin: "0 10px",
+                          color: "#e4f0fa",
+                          width: "20px",
+                          height: "20px",
+                        }}
+                      />
+                    </button>
+                  </a>
+                </div>
+              ))}
+            </div>
+          </MyContainer>
+          <Footer />
+        </OuterloginContainer>
+      </>
+    </Layout>
   );
 };
 export default UpdateDetails;
