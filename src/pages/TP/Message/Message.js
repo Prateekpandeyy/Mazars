21c02b97a4c9aecc03e871d703317271f801f79c
Prateek <Pandeyprateek528@gmail.com>
2023-03-27 15:01:21 +0530

    done

--- src/pages/TP/Message/Message.js
+++ src/pages/TP/Message/Message.js
@@ -1,17 +1,193 @@
+// import React, { useState, useEffect } from "react";
+// import Layout from "../../../components/Layout/Layout";
+// import axios from "axios";
+// import { baseUrl } from "../../../config/config";
+
+// import {
+//   Card,
+//   CardHeader,
+//   CardBody,
+//   CardTitle,
+//   Row,
+//   Col,
+//   Table,
+// } from "reactstrap";
+// import { Link } from "react-router-dom";
+// import BootstrapTable from "react-bootstrap-table-next";
+// import PaymentModal from "./PaymentModal";
+// import CommonServices from "../../../common/common";
+// import { useHistory } from "react-router";
+// import DataTablepopulated from "../../../components/DataTablepopulated/DataTabel";
+// function Message(props) {
+//   const userId = window.localStorage.getItem("tpkey");
+//   const [query, setQuery] = useState([]);
+//   const [data, setData] = useState(null);
+//   const history = useHistory();
+//   const [addPaymentModal, setPaymentModal] = useState(false);
+//   const token = window.localStorage.getItem("tptoken");
+//   const myConfig = {
+//     headers: {
+//       uit: token,
+//     },
+//   };
+//   const paymentHandler = (key) => {
+//     setPaymentModal(!addPaymentModal);
+//   };
+
+//   useEffect(() => {
+//     getMessage();
+//   }, []);
+
+//   const getMessage = () => {
+//     axios
+//       .get(`${baseUrl}/tl/getNotification?id=${JSON.parse(userId)}`, myConfig)
+//       .then((res) => {
+//         if (res.data.code === 1) {
+//           setQuery(res.data.result);
+//         }
+//       });
+//   };
+
+//   const columns = [
+//     {
+//       text: "S.No",
+//       dataField: "",
+//       formatter: (cellContent, row, rowIndex) => {
+//         return rowIndex + 1;
+//       },
+//       headerStyle: () => {
+//         return { fontSize: "12px", width: "30px" };
+//       },
+//     },
+
+//     {
+//       text: "Date",
+//       dataField: "setdate",
+//       sort: true,
+//       headerStyle: () => {
+//         return { fontSize: "12px", width: "60px" };
+//       },
+//     },
+
+//     {
+//       text: "Query No",
+//       dataField: "assign_no",
+//       headerStyle: () => {
+//         return { fontSize: "12px", width: "30px" };
+//       },
+//       formatter: function nameFormatter(cell, row) {
+//         return (
+//           <>
+//             {/* <Link to={`/customer/my-assingment/${row.id}`}> */}
+//             {row.assign_no}
+//             {/* </Link> */}
+//           </>
+//         );
+//       },
+//     },
+//     {
+//       text: "Message",
+//       sort: true,
+//       headerStyle: () => {
+//         return { fontSize: "12px", width: "180px" };
+//       },
+//       formatter: function nameFormatter(cell, row) {
+//         return (
+//           <>
+//             <Link to={`/taxprofessional_view-notification/${row.id}`}>
+//               {row.is_read == "0" ? (
+//                 <div
+//                   style={{
+//                     cursor: "pointer",
+//                     display: "flex",
+//                     justifyContent: "space-between",
+//                     wordBreak: "break-word",
+//                   }}
+//                   onClick={() => readNotification(row.id)}
+//                   title="unread"
+//                 >
+//                   <p>{row.message}</p>
+//                   <i class="fa fa-bullseye" style={{ color: "red" }}></i>
+//                 </div>
+//               ) : (
+//                 <div
+//                   style={{
+//                     cursor: "pointer",
+//                     display: "flex",
+//                     justifyContent: "space-between",
+//                     wordBreak: "break-word",
+//                   }}
+//                   title="read"
+//                 >
+//                   <p>{row.message}</p>
+//                   <i class="fa fa-bullseye" style={{ color: "green" }}></i>
+//                 </div>
+//               )}
+//             </Link>
+//           </>
+//         );
+//       },
+//     },
+//   ];
+
+//   // readnotification
+//   const readNotification = (id) => {
+//     axios
+//       .get(`${baseUrl}/tl/markReadNotification?id=${id}`, myConfig)
+//       .then(function (response) {})
+//       .catch((error) => {});
+//   };
+
+//   return (
+//     <Layout TPDashboard="TPDashboard" TPuserId={userId}>
+//       <Card>
+//         <CardHeader>
+//           <Row>
+//             <Col md="4">
+//               <button class="autoWidthBtn" onClick={() => history.goBack()}>
+//                 Go Back
+//               </button>
+//             </Col>
+//             <Col md="8">
+//               <h4>Message</h4>
+//             </Col>
+//           </Row>
+//         </CardHeader>
+//         <CardBody
+//           style={{ display: "flex", height: "80vh", overflowY: "scroll" }}
+//         >
+//           <DataTablepopulated
+//             bgColor="#42566a"
+//             keyField={"assign_no"}
+//             data={query}
+//             columns={columns}
+//           ></DataTablepopulated>
+//           <PaymentModal
+//             paymentHandler={paymentHandler}
+//             addPaymentModal={addPaymentModal}
+//           />
+//         </CardBody>
+//       </Card>
+//     </Layout>
+//   );
+// }
+
+// export default Message;
 import React, { useState, useEffect } from "react";
 import Layout from "../../../components/Layout/Layout";
 import axios from "axios";
 import { baseUrl } from "../../../config/config";
 
+import { Card, CardHeader, CardBody, CardTitle, Row, Col } from "reactstrap";
 import {
-  Card,
-  CardHeader,
-  CardBody,
-  CardTitle,
-  Row,
-  Col,
-  Table,
-} from "reactstrap";
+  // Table,
+  // TableContainer,
+  // TableHead,
+  TablePagination,
+  // TableBody,
+  // TableRow,
+  // TableCell,
+} from "@material-ui/core";
 import { Link } from "react-router-dom";
 import BootstrapTable from "react-bootstrap-table-next";
 import PaymentModal from "./PaymentModal";
@@ -20,40 +196,167 @@ import { useHistory } from "react-router";
 import DataTablepopulated from "../../../components/DataTablepopulated/DataTabel";
 function Message(props) {
   const userId = window.localStorage.getItem("tpkey");
+
   const [query, setQuery] = useState([]);
-  const [data, setData] = useState(null);
+  const [atPage, setAtpage] = useState(1);
+  const [allId, setAllId] = useState([]);
+  const [rowsPerPage, setRowsPerPage] = useState(50);
+  const [big, setBig] = useState(1);
+  const [end, setEnd] = useState(50);
   const history = useHistory();
   const [addPaymentModal, setPaymentModal] = useState(false);
   const token = window.localStorage.getItem("tptoken");
+  const [countNotification, setCountNotification] = useState("");
+  const [totalPages, setTotalPages] = useState(1);
+  const [page, setPage] = useState(0);
+  const defaultPage = ["1", "2", "3", "4", "5"];
   const myConfig = {
     headers: {
       uit: token,
     },
   };
+
   const paymentHandler = (key) => {
     setPaymentModal(!addPaymentModal);
+  };
+  const loadMessage = (e) => {
+    let clickedId = allId.filter((i) => {
+      return i === e;
+    });
+
+    if (clickedId.length === 0 && isNaN(e) === false && e > 1) {
+      axios
+        .get(`${baseUrl}/tl/getNotification?page=${e}`, myConfig)
+        .then((res) => {
+          if (res.data.code === 1) {
+            let data = query.concat(res.data.result);
+            let all = [];
+            let customId = 1;
+            data.map((i) => {
+              let data = {
+                ...i,
+                cid: customId,
+              };
+              customId++;
+              all.push(data);
+            });
+            setQuery(all);
+
+            setAllId((payload) => {
+              return [...payload, e];
+            });
+          }
+        });
+    }
   };
 
   useEffect(() => {
     getMessage();
+
+    setEnd(50);
   }, []);
 
+  const onChangeRowsPerPage = (e) => {
+    setRowsPerPage(e.target.value);
+  };
+
+  const chunkSize = 24;
+
+  const sliceIntoChunks = (query, chunkSize) => {
+    const res = [];
+    for (let i = 0; i < query.length; i += chunkSize) {
+      const chunk = query.slice(i, i + chunkSize);
+      res.push(chunk);
+    }
+    return res;
+  };
+  const divided = [];
+  divided.push(sliceIntoChunks(query, chunkSize));
+  const un = divided[0];
+
+  // const totalMsg = () => {
+  //   axios
+  //     .get(
+  //       `${baseUrl}/tl/getNotification?id=${JSON.parse(
+  //         userId
+  //       )}&type_list=uread`,
+  //       myConfig
+  //     )
+  //     .then((res) => {
+  //       if (res.data.code === 1) {
+  //         setCountNotification(res.data.result[0].total);
+  //       }
+  //     });
+  // };
+
+  useEffect(() => {
+    const totalP = Math.ceil(countNotification / 50);
+    setTotalPages(totalP);
+    setEnd(50);
+  }, [countNotification]);
+
+  // set intial query here
   const getMessage = () => {
     axios
-      .get(`${baseUrl}/tl/getNotification?id=${JSON.parse(userId)}`, myConfig)
+      .get(
+        `${baseUrl}/tl/getNotification?id=${JSON.parse(userId)}&page=1`,
+        myConfig
+      )
+      .then((res) => {
+        if (res.data.code === 1) {
+          setQuery(res.data.result);
+          setCountNotification(res.data.result.length);
+        }
+      });
+  };
+
+  //page counter
+  const firstChunk = () => {
+    setAtpage(1);
+    setPage(1);
+  };
+  const prevChunk = () => {
+    if (atPage > 1) {
+      setAtpage((atPage) => atPage - 1);
+    }
+    setPage(page - 1);
+  };
+  const nextChunk = () => {
+    if (atPage < totalPages) {
+      setAtpage((atPage) => atPage + 1);
+    }
+    setPage(page + 1);
+  };
+  const lastChunk = () => {
+    setPage(defaultPage.at(-1));
+    setAtpage(totalPages);
+  };
+  //page counter Ends here
+
+  //call for more notification on page Change
+  useEffect(() => {
+    // console.log(atPage);
+    setBig(50 * (atPage - 1) + 1);
+    if (atPage == totalPages) {
+      setEnd(countNotification);
+    } else {
+      setEnd(50 * atPage);
+    }
+    axios
+      .get(`${baseUrl}/tl/getNotification?page=${atPage}`, myConfig)
       .then((res) => {
         if (res.data.code === 1) {
           setQuery(res.data.result);
         }
       });
-  };
+  }, [atPage]);
 
   const columns = [
     {
       text: "S.No",
       dataField: "",
       formatter: (cellContent, row, rowIndex) => {
-        return rowIndex + 1;
+        return rowIndex + 1 * ((atPage - 1) * 50) + 1;
       },
       headerStyle: () => {
         return { fontSize: "12px", width: "30px" };
@@ -148,8 +451,64 @@ function Message(props) {
                 Go Back
               </button>
             </Col>
-            <Col md="8">
+            <Col md="2">
               <h4>Message</h4>
+            </Col>
+            <Col md="6">
+              <div className="customPagination">
+                <div className="mx-2 d-flex w-100 align-items-center">
+                  <span>
+                    {big}-{end} of {countNotification}
+                  </span>
+                  <span className="d-flex">
+                    <button
+                      className="navButton mx-1"
+                      onClick={(e) => firstChunk()}
+                    >
+                      &lt; &lt;
+                    </button>
+
+                    <button
+                      className="navButton mx-1"
+                      onClick={(e) => prevChunk()}
+                    >
+                      &lt;
+                    </button>
+                    <div
+                      style={{
+                        display: "flex",
+                        maxWidth: "100px",
+                        width: "100%",
+                      }}
+                    >
+                      <select
+                        value={page}
+                        onChange={(e) => {
+                          setPage(e.target.value);
+                          loadMessage(e.target.value);
+                        }}
+                        className="form-control"
+                      >
+                        {defaultPage.map((i) => (
+                          <option value={i}>{i}</option>
+                        ))}
+                      </select>
+                    </div>
+                    <button
+                      className="navButton mx-1"
+                      onClick={(e) => nextChunk()}
+                    >
+                      &gt;
+                    </button>
+                    <button
+                      className="navButton mx-1"
+                      onClick={(e) => lastChunk()}
+                    >
+                      &gt; &gt;
+                    </button>
+                  </span>
+                </div>
+              </div>
             </Col>
           </Row>
         </CardHeader>
