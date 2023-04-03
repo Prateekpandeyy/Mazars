56c795f22b1b6d137507ffaaedc5d5663fad307f
Prateek <Pandeyprateek528@gmail.com>
2023-04-03 09:56:34 +0530

    done

--- src/pages/Admin/PaymentStatusTab/Unpaid.js
+++ src/pages/Admin/PaymentStatusTab/Unpaid.js
@@ -168,6 +168,9 @@ function Unpaid() {
       )
       .then((res) => {
         if (res.data.code === 1) {
+          setPage(1);
+          setBig(1);
+          setEnd(Number(localStorage.getItem("admin_record_per_page")));
           let all = [];
           let sortId = 1;
           if (page > 1) {
