e2e611860fa9284aaf1a3a4ca4a9c4372a35cab4
Shubhanshu Pandey <shubhanshu.p@multitvsolution.com>
2023-04-26 16:08:51 +0530

    done

--- src/components/Search-Filter/InvoiceFilter.js
+++ src/components/Search-Filter/InvoiceFilter.js
@@ -240,6 +240,7 @@ const InvoiceFilter = (props) => {
               placeholder="Enter Query Number"
               className="form-control"
               defaultValue={queryNo}
+              value={queryNo}
               onChange={(e) => setQueryNo(e.target.value)}
             />
           </div>
@@ -398,8 +399,8 @@ const InvoiceFilter = (props) => {
             ""
           )}
           {props.invoice == "tlcreate" ||
-          props.invoice === "tpcreate" ||
-          props.invoice === "admincreate" ? (
+            props.invoice === "tpcreate" ||
+            props.invoice === "admincreate" ? (
             ""
           ) : (
             <>
