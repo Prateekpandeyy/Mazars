f4f4b6e24372efa12e2f7b68ddde6447b1fdb0b0
Shubhanshu Pandey <shubhanshu.p@multitvsolution.com>
2023-05-17 18:09:18 +0530

    changes

--- src/components/Paginator/Paginator.js
+++ src/components/Paginator/Paginator.js
@@ -677,13 +677,13 @@ function Paginator(props) {
             let field = pagetry?.field;
             localStorage.setItem(`tpInvoice1`, JSON.stringify(e));
             if ((data) && (!pagetry)) {
-                if (data.installment_no === undefined) {
+                if (data.installment_no) {
                     remainApiPath = `tl/getPaymentDetail?page=${e}&invoice=1&qno=${data.query_no}&payment_plan=${data.payment_plan}&from=${data.p_dateFrom}&to=${data.p_dateTo}&status=${data.opt}`
                 } else {
                     remainApiPath = `tl/getPaymentDetail?page=${e}&invoice=1&qno=${data.query_no}&payment_plan=${data.payment_plan}&from=${data.p_dateFrom}&to=${data.p_dateTo}&status=${data.opt}&installment_no=${data?.installment_no}`
                 }
             } else if ((data && Object.values(data).length > 0) && (pagetry)) {
-                if (data.installment_no === undefined) {
+                if (data.installment_no) {
                     remainApiPath = `tl/getPaymentDetail?page=${e}&invoice=1&qno=${data.query_no}&payment_plan=${data.payment_plan}&from=${data.p_dateFrom}&to=${data.p_dateTo}&status=${data.opt}&orderby=${val}&orderbyfield=${field}`
                 } else {
                     remainApiPath = `tl/getPaymentDetail?page=${e}&invoice=1&qno=${data.query_no}&payment_plan=${data.payment_plan}&from=${data.p_dateFrom}&to=${data.p_dateTo}&status=${data.opt}&installment_no=${data?.installment_no}&orderby=${val}&orderbyfield=${field}`
