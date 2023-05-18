b10d109c6fd20b12499247c598998cdecf27b158
Shubhanshu Pandey <shubhanshu.p@multitvsolution.com>
2023-05-18 12:36:45 +0530

    changes

--- src/pages/TP/Invoice/Generated.js
+++ src/pages/TP/Invoice/Generated.js
@@ -30,8 +30,8 @@ const useStyles = makeStyles((theme) => ({
 const Generated = () => {
   var rowStyle2 = {};
   const userid = window.localStorage.getItem("tpkey");
-  // const allEnd = Number(localStorage.getItem("tp_record_per_page"));
-  const allEnd = 5;
+  const allEnd = Number(localStorage.getItem("tp_record_per_page"));
+  // const allEnd = 5;
   const classes = useStyles();
   const [records, setRecords] = useState([]);
   const [proposal, setProposal] = useState([]);
