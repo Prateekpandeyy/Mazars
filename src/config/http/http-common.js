eba8c1e60a0985fdcc6d3f5afd392ad894a726e7
Prateek <Pandeyprateek528@gmail.com>
2023-05-24 10:53:42 +0530

    done

--- src/config/http/http-common.js
+++ src/config/http/http-common.js
@@ -2,16 +2,16 @@ import axios from "axios";
 
 // //for development
 
+// export default axios.create({
+//   baseURL: "https://stagingapi-advisorysolutions.mazars.co.in/v1",
+//   headers: {
+//     "Content-type": "application/json",
+//   },
+//});
+// // // //for server
 export default axios.create({
-  baseURL: "https://stagingapi-advisorysolutions.mazars.co.in/v1",
+  baseURL: "https://api-advisorysolutions.mazars.co.in/v1",
   headers: {
     "Content-type": "application/json",
   },
 });
-// // // //for server
-// export default axios.create({
-//   baseURL: "https://api-advisorysolutions.mazars.co.in/v1",
-//   headers: {
-//     "Content-type": "application/json",
-//   },
-// });
