e3451cbb01fe39a2384d6675c349794321f03b47
Shubhanshu Pandey <shubhanshu.p@multitvsolution.com>
2023-07-05 11:48:46 +0530

    changes

--- src/pages/TP/Invoice/CreateInvoice.js
+++ src/pages/TP/Invoice/CreateInvoice.js
@@ -147,17 +147,17 @@ const CreateInvoice = () => {
 
       fieldBy = pagetry.fieldBy;
     }
-    if((searchData) && (!searchData.installment_no)){
-      console.log('This');
-    }
     if (searchData) {
-      if(!searchData.installment_no){
-        remainApiPath = `tl/getPaymentDetail?&invoice=0&page=${e}&orderby=${orderBy}&orderbyfield=${fieldBy}&query_no=${searchData.query_no}&payment_plan=${searchData?.payment_plan}`;
-      }else{
-        remainApiPath = `tl/getPaymentDetail?&invoice=0&page=${e}&installment_no=${searchData.installment_no}&orderby=${orderBy}&orderbyfield=${fieldBy}&query_no=${searchData.query_no}&payment_plan=${searchData?.payment_plan}`;
-      }
+        remainApiPath = `tl/getPaymentDetail?tp_id=${JSON.parse(
+          userid
+        )}&invoice=0&page=${e
+        }&orderby=${orderBy}&orderbyfield=${fieldBy}&query_no=${searchData.query_no
+        }&installment_no=${searchData.installment_no !== undefined ? searchData.installment_no : ""}&payment_plan=${
+          searchData.payment_plan !== undefined ? searchData.payment_plan: ""}`;
     } else {
-      remainApiPath = `tl/getPaymentDetail?&invoice=0&page=${e}&orderby=${orderBy}&orderbyfield=${fieldBy}`;
+      remainApiPath = `tl/getPaymentDetail?tp_id=${JSON.parse(
+        userid
+      )}&invoice=0&page=${e}&orderby=${orderBy}&orderbyfield=${fieldBy}`;
     }
 
     // localStorage.setItem(`tpInvoice1`, JSON.stringify(e));
@@ -293,12 +293,10 @@ const CreateInvoice = () => {
     let searchData = JSON.parse(localStorage.getItem("tpcreate"));
 
     if (searchData?.installment_no || searchData?.opt || searchData?.query_no) {
-      if (!searchData?.installment_no && searchData?.payment_plan) {
-      } else if (searchData?.installment_no && searchData?.payment_plan) {
-        remainApiPath = `tl/getPaymentDetail?&invoice=0&page=1&orderby=${val}&orderbyfield=${field}&query_no=${searchData.query_no}&payment_plan=${searchData?.payment_plan}&installment_no=${searchData?.installment_no}`;
-      } else {
-        remainApiPath = `tl/getPaymentDetail?&page=1&invoice=0&qno=${searchData.query_no}&orderby=${val}&orderbyfield=${field}`;
-      }
+        remainApiPath = `tl/getPaymentDetail?tp_id=${JSON.parse(
+          userid)}&invoice=0&page=1&orderby=${val}&orderbyfield=${
+        field}&query_no=${searchData.query_no}&installment_no=${searchData.installment_no !== undefined ? searchData.installment_no : ""}&payment_plan=${
+          searchData.payment_plan !== undefined ? searchData.payment_plan: ""}`;
     } else {
       remainApiPath = `tl/getPaymentDetail?page=1&tp_id=${JSON.parse(
         userid
