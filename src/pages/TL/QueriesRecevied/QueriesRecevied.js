b44a8a8dd3ca93ce27d910733a29fd18ac3fa3c2
Prateek <Pandeyprateek528@gmail.com>
2023-07-11 10:48:08 +0530

    done

--- src/pages/TL/QueriesRecevied/QueriesRecevied.js
+++ src/pages/TL/QueriesRecevied/QueriesRecevied.js
@@ -203,7 +203,7 @@ function QueriesRecevied(props) {
                   {props.location.index ? (
                     <Link
                       to={{
-                        pathname: `/admin/${props.location.routes}`,
+                        pathname: `/teamleader/${props.location.routes}`,
                         index: props.location.index,
                       }}
                     >
